0ff27ef8ea7b499f2164faddda592ec25244e785
HotsixMan <hotsixman05@gmail.com>
2024-06-22 18:04:02 +0900

    레이팅 추가

---
+++ src/lib/components/user.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	export let userData: {
+		nickname: string;
+		taikoNumber: number;
+		myDon: string;
+	};
+</script>
+
+<div class="container">
+    <img src={userData.myDon} alt=""/>
+    <table>
+        <tr>
+            <td>
+                닉네임
+            </td>
+            <td>
+                {userData.nickname}
+            </td>
+        </tr>
+        <tr>
+            <td>
+                북 번호
+            </td>
+            <td>
+                {userData.taikoNumber}
+            </td>
+        </tr>
+    </table>
+</div>
+
+<style>
+    .container{
+        width: min(100%, 300px);
+
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    img{
+        width: min(100%, 200px);
+        height: auto;
+    }
+
+    table{
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    td{
+        text-align: center;
+        border: 1px solid black;
+    }
+</style>
--- src/routes/analyze/+page.svelte
+++ src/routes/analyze/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import Rating from '$lib/components/rating.svelte';
+    import User from '$lib/components/user.svelte';
 	import { error } from '@sveltejs/kit';
+    import { csv2json } from 'json-2-csv';
 
-    export let form;
+	export let form;
 
     if(!form){
         throw error(400);
@@ -9,5 +12,12 @@
 
     const {data} = form;
 
-    console.log(data);
+    const measures = fetch('https://raw.githubusercontent.com/taikowiki/taiko-fumen-measure-table/main/main.csv')
+                    .then(data => data.text())
+                    .then(text => csv2json(text))
 </script>
+
+<User userData={data.userData} />
+{#await measures then measures}
+    <Rating {measures} scoreDatas={data.scoreDatas}/>
+{/await}
