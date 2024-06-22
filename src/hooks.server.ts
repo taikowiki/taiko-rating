59480b71caee97448eeca563585911dd6eb19514
HotsixMan <hotsixman05@gmail.com>
2024-06-22 21:02:56 +0900

    initalize

--- src/hooks.server.ts
+++
@@ -1,3 +0,0 @@
-import allowOrigin from "$lib/module/server/hooks/allow-origin";
-
-export const handle = allowOrigin(['https://donderhiroba.jp'])
--- src/routes/+page.server.ts
+++
@@ -1,5 +0,0 @@
-export async function load({url}){
-    return{
-        code: `(async() => {const fetched = await fetch('${url.origin}/rating.js');const script = await fetched.text();(new Function(script))();})();`
-    }
-}
