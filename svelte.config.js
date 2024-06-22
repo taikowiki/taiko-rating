5dacbb0cbfc9f20ee3244d1c660ec0947f630bff
HotsixMan <hotsixman05@gmail.com>
2024-06-22 15:47:54 +0900

    .

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		csrf: process.env.NODE_ENV === 'development' ? false : true
+		csrf: {
+			checkOrigin: process.env.NODE_ENV === 'development' ? false : true
+		}
 	}
 };
 
