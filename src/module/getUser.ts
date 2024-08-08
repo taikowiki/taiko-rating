187732ebd643114b503b0d6bdba65255f2f81fdd
HotsixMan <hotsixman05@gmail.com>
2024-08-08 12:09:51 +0900

    init

--- src/module/getUser.ts
+++
@@ -1,9 +0,0 @@
-import { parse } from "node-hiroba";
-
-export async function getUser() {
-    const fetched = await fetch('https://donderhiroba.jp');
-    const body = await fetched.text();
-    const parsed = parse.parseCurrentLogin(body);
-    parsed.nickname = parsed.nickname.replace(/\n/g, '').replace(/\t/g, '');
-    return parsed;
-}
