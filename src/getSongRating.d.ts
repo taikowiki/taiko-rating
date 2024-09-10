2c47daa08a9509cb29280727062d122dc2dfdd02
HotsixMan <hotsixman05@gmail.com>
2024-09-10 20:18:11 +0900

    .

--- src/getSongRating.d.ts
+++
@@ -1,8 +0,0 @@
-import type { DifficultyScoreData, Crown } from 'node-hiroba/types';
-export declare function getSongRating(difficultyScoreData: DifficultyScoreData, maxCombo: number, measure: number): {
-    value: number;
-    accuracy: number;
-};
-export declare function getCrownBonus(crown: Crown): 1 | 0.7 | 1.1 | 1.15 | 0;
-export declare function getCompensated(accuracy: number): number;
-//# sourceMappingURL=getSongRating.d.ts.map
